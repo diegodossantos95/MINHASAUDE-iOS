0c42e541213fc18d882030ee6dddb2656ae4a572
dos Santos, Diego <diego.dos.santos@sap.com>
2019-08-16 00:09:16 -0300

    Added basic code for Auth

--- MeuNutri/ViewController.swift
+++ MeuNutri/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+//        Auth.auth().addStateDidChangeListener { (auth, user) in
+//        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        Auth.auth().signIn(withEmail: "santos.diegod@gmail.com", password: "di5e4go6") { (auth, error) in
+            print(auth)
+            print(error)
+
+        }
     }
 
 
